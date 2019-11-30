d298bc48726f364627cb0676c3f470d85837a758
jithware <jithware@jithware.com>
2019-11-30 16:36:43 -0500

    added more cat

--- catdog/catdog.go
+++ catdog/catdog.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,20 +10,29 @@ import (
 
 // Usage prints usage
 func Usage() {
-	log.Fatal("Usage: catdog <file>")
+	fmt.Println("Usage: " + os.Args[0] + " [OPTION]... [FILE]...")
+	os.Exit(0)
 }
 
 func main() {
 
-	if len(os.Args) != 2 {
+	if len(os.Args) == 1 {
 		Usage()
 	}
 
-	arg := os.Args[1]
-	content, err := ioutil.ReadFile(arg)
-	if err != nil {
-		log.Fatal(err)
+	help := flag.Bool("help", false, "display this help and exit")
+	flag.Parse()
+	if *help {
+		Usage()
 	}
 
-	fmt.Printf("%s", content)
+	args := os.Args[1:]
+	for _, arg := range args {
+		content, err := ioutil.ReadFile(arg)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			fmt.Printf("%s", content)
+		}
+	}
 }
